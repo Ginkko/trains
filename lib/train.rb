9b3b05b9650b9fb31e42a24b4a5b1ce7a0685eb4
Alec Arme & Brent Bailey <student@epicodus.com>
2015-05-06 10:42:46 -0700

    add delete function, pass delete spec

--- lib/train.rb
+++ lib/train.rb
@@ -38,5 +38,9 @@ class Train
     DB.exec("UPDATE trains SET name = '#{@name}' WHERE id = #{@id};")
   end
 
+  def delete
+    DB.exec("DELETE FROM trains WHERE id = #{self.id()};")
+  end
+
 
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -17,12 +17,24 @@ describe(Train) do
       expect(Train.find(test_train.id)).to eq(test_train)
     end
   end
+
   describe('#update') do
     it("lets you update trains in the database") do
       test_train = Train.new({:id => nil, :name => "Red Line"})
       test_train.save
       test_train.update({:name => "Green Line"})
       expect(test_train.name()).to eq("Green Line")
+    end
+  end
+
+  describe('#delete') do
+    it('lets you delete a train from the database') do
+      test_train = Train.new({:id => nil, :name => "Red Line"})
+      test_train.save
+      test_train2 = Train.new({:id => nil, :name => "Green Line"})
+      test_train2.save
+      test_train.delete()
+      expect(Train.all).to eq([test_train2])
     end
   end
 
